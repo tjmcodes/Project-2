5f48dc8ea5cea16a64c37117ba1f3dd0e68a9a2c
robynsm <robynamysmith92@gmail.com>
2022-05-19 22:02:49 +0100

    Added styling on buttons on homepage to remove underline

--- src/components/CocktailsList.js
+++ src/components/CocktailsList.js
@@ -9,19 +9,19 @@ function CocktailsList() {
   return <div className="container">
     <h1>Gin</h1>
     <GinCocktails />
-    <span className="link"><button className="button is-danger is-light"><Link to="/allgin">{"All Gin Cocktails"}</Link></button></span>
+    <span className="link"><button className="button is-danger is-light"><Link to="/allgin" style={{ textDecoration: "none" }}>{"All Gin Cocktails"}</Link></button></span>
 
     <h1>Rum</h1>
     <RumCocktails />    
-    <span className="link"><button className="button is-danger is-light"><Link to="/allrum">{"All Rum Cocktails"}</Link></button></span>
+    <span className="link"><button className="button is-danger is-light"><Link to="/allrum" style={{ textDecoration: "none" }}>{"All Rum Cocktails"}</Link></button></span>
 
     <h1>Vodka</h1>
     <VodkaCocktails />
-    <span className="link"><button className="button is-danger is-light"><Link to="/allvodka">{"All Vodka Cocktails"}</Link></button></span>
+    <span className="link"><button className="button is-danger is-light"><Link to="/allvodka" style={{ textDecoration: "none" }}>{"All Vodka Cocktails"}</Link></button></span>
     
     <h1>Tequila</h1>
     <TequilaCocktails />
-    <span className="link"><button className="button is-danger is-light"><Link to="/alltequila">{"All Tequila Cocktails"}</Link></button></span>
+    <span className="link"><button className="button is-danger is-light"><Link to="/alltequila" style={{ textDecoration: "none" }}>{"All Tequila Cocktails"}</Link></button></span>
   </div>
   
 }
